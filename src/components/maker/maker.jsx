d38bb1482081c623c156f2f79acd4d331085e182
Sukkyeong-Park <skpark@voistore.com>
2021-10-20 14:25:12 +0900

    handle state change on editor

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,41 +7,42 @@ import Editor from '../editor/editor';
 import Preview from '../preview/preview';
 
 const Maker = ({authService}) => {
-    const [cards, setCards] = useState([
+    const [cards, setCards] = useState(
         {
-            id: '1',
-            name: 'Ellie',
-            company: 'Samsung',
-            theme: 'dark',
-            title: 'Software Engineer',
-            email: 'ellie@gmail.com',
-            message: 'go for it',
-            fileName: 'ellie',
-            fileURL:null
-        },
-        {
-            id: '2',
-            name: 'Ellie2',
-            company: 'Samsung',
-            theme: 'light',
-            title: 'Software Engineer',
-            email: 'ellie@gmail.com',
-            message: 'go for it',
-            fileName: 'ellie',
-            fileURL:'ellie.png'
-        },
-        {
-            id: '3',
-            name: 'Ellie3',
-            company: 'Samsung',
-            theme: 'colorful',
-            title: 'Software Engineer',
-            email: 'ellie@gmail.com',
-            message: 'go for it',
-            fileName: 'ellie',
-            fileURL:null
-        }                
-    ]);
+            '1': {
+                id: '1',
+                name: 'Ellie',
+                company: 'Samsung',
+                theme: 'dark',
+                title: 'Software Engineer',
+                email: 'ellie@gmail.com',
+                message: 'go for it',
+                fileName: 'ellie',
+                fileURL:null
+            },
+            '2':{
+                id: '2',
+                name: 'Ellie2',
+                company: 'Samsung',
+                theme: 'light',
+                title: 'Software Engineer',
+                email: 'ellie@gmail.com',
+                message: 'go for it',
+                fileName: 'ellie',
+                fileURL:'ellie.png'
+            },
+            '3':{
+                id: '3',
+                name: 'Ellie3',
+                company: 'Samsung',
+                theme: 'colorful',
+                title: 'Software Engineer',
+                email: 'ellie@gmail.com',
+                message: 'go for it',
+                fileName: 'ellie',
+                fileURL:null
+            }
+        });
 
     const history = useHistory();
     const onLogout = () => {
@@ -55,16 +56,30 @@ const Maker = ({authService}) => {
             }
         });
     });
-    
-    const addCard = card => {
-        const updated =[...cards, card];
-        setCards(updated);
+
+    const createOrUpdateCad = card => {
+        setCards(cards => {
+            const updated = {...cards};
+            updated[card.id] = card;
+            return updated;
+        });
+    };
+
+    const deleteCard = card => {
+        setCards(cards => {
+            const updated = {...cards};
+            delete updated[card.id];
+            return updated;
+        });
     };
     return(
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
             <div className={styles.container}>
-                <Editor cards={cards} addCard={addCard}/>
+                <Editor cards={cards} 
+                addCard={createOrUpdateCad} 
+                updateCard={createOrUpdateCad} 
+                deleteCard={deleteCard} />
                 <Preview cards={cards}/>
             </div>
             <Footer />
