a86a9d5326defad81b4fe99acd04301a52e2d7b0
Sukkyeong-Park <skpark@voistore.com>
2021-10-21 12:04:05 +0900

    handle image uploading

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,50 +1,107 @@
-import React, {useRef} from 'react';
-import Button from '../button/button';
-import ImageFileInput from '../image_file_input/image_file_input';
+import React, { useRef } from 'react';
 import styles from './card_edit_form.module.css';
+import Button from '../button/button';
 
-const CardEditForm = ({ card, updateCard, deleteCard }) => {
-    const {name, company, title, email, message, theme, filename, fileURL} = card;
-    
-    const onChange = (event) => {
-        if (event.currentTarget == null){
-            return;
-        }
-        event.preventDefault();
-        updateCard({
-            ...card,
-            [event.currentTarget.name]: event.currentTarget.value,
-        }
-        )
+const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
+  const nameRef = useRef();
+  const companyRef = useRef();
+  const themeRef = useRef();
+  const titleRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
+
+  const {
+    name,
+    company,
+    title,
+    email,
+    message,
+    theme,
+    fileName,
+    fileURL,
+  } = card;
+
+  const onFileChange = file => {
+    updateCard({
+      ...card,
+      fileName: file.name,
+      fileURL: file.url,
+    });
+  };
+
+  const onChange = event => {
+    if (event.currentTarget == null) {
+      return;
     }
-    const onSubmit = () => {
-        deleteCard(card);
-    };
+    event.preventDefault();
+    updateCard({
+      ...card,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
 
-    const formRef = useRef();
-    const nameRef = useRef();
-    const companyRef = useRef();
-    const themeRef = useRef();
-    const titleRef = useRef();
-    const emailRef = useRef();
-    const messageRef = useRef();
+  const onSubmit = () => {
+    deleteCard(card);
+  };
 
-    return (
-        <form className={styles.form}>
-            <input ref={nameRef} onChange={onChange} className={styles.input} type="text" name="name" value={name}></input>
-            <input ref={companyRef} onChange={onChange} className={styles.input} type="text" name="company" value={company}></input>
-            <select ref={themeRef} onChange={onChange} className={styles.select} name="theme" value={theme}>
-                <option value="light">Light</option>
-                <option value="dark">Dark</option>
-                <option value="colorful">Colorful</option>
-            </select>
-            <input ref={titleRef} onChange={onChange} className={styles.input} type="text" name="title" value={title}></input>
-            <input ref={emailRef} onChange={onChange} className={styles.input} type="text" name="email" value={email}></input>
-            <textarea ref={messageRef} onChange={onChange} className={styles.textarea} name="message" value={message}></textarea>
-            <div className={styles.fileInput}><ImageFileInput /></div>
-            <Button name="Delete" onClick={onSubmit} />
-        </form>
-    )
+  return (
+    <form className={styles.form}>
+      <input
+        className={styles.input}
+        type="text"
+        name="name"
+        ref={nameRef}
+        value={name}
+        onChange={onChange}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="company"
+        ref={companyRef}
+        value={company}
+        onChange={onChange}
+      />
+      <select
+        className={styles.select}
+        name="theme"
+        ref={themeRef}
+        value={theme}
+        onChange={onChange}
+      >
+        <option value="light">light</option>
+        <option value="dark">dark</option>
+        <option value="colorful">colorful</option>
+      </select>
+      <input
+        className={styles.input}
+        type="text"
+        name="title"
+        ref={titleRef}
+        value={title}
+        onChange={onChange}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="email"
+        ref={emailRef}
+        value={email}
+        onChange={onChange}
+      />
+      <textarea
+        className={styles.textarea}
+        ref={messageRef}
+        name="message"
+        value={message}
+        onChange={onChange}
+      />
+      <div className={styles.fileInput}>
+        <FileInput name={name} onFileChange={onFileChange} />
+      </div>
+      <Button name="Delete" onClick={onSubmit} />
+    </form>
+  );
 };
 
 export default CardEditForm;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,91 +1,93 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import Header from '../header/header';
 import Footer from '../footer/footer';
-import styles from './maker.module.css';
+import Header from '../header/header';
 import Editor from '../editor/editor';
 import Preview from '../preview/preview';
+import styles from './maker.module.css';
 
-const Maker = ({authService}) => {
-    const [cards, setCards] = useState(
-        {
-            '1': {
-                id: '1',
-                name: 'Ellie',
-                company: 'Samsung',
-                theme: 'dark',
-                title: 'Software Engineer',
-                email: 'ellie@gmail.com',
-                message: 'go for it',
-                fileName: 'ellie',
-                fileURL:null
-            },
-            '2':{
-                id: '2',
-                name: 'Ellie2',
-                company: 'Samsung',
-                theme: 'light',
-                title: 'Software Engineer',
-                email: 'ellie@gmail.com',
-                message: 'go for it',
-                fileName: 'ellie',
-                fileURL:'ellie.png'
-            },
-            '3':{
-                id: '3',
-                name: 'Ellie3',
-                company: 'Samsung',
-                theme: 'colorful',
-                title: 'Software Engineer',
-                email: 'ellie@gmail.com',
-                message: 'go for it',
-                fileName: 'ellie',
-                fileURL:null
-            }
-        });
+const Maker = ({ FileInput, authService }) => {
+  const [cards, setCards] = useState({
+    1: {
+      id: '1',
+      name: 'Ellie',
+      company: 'Samsung',
+      theme: 'dark',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+    2: {
+      id: '2',
+      name: 'Ellie2',
+      company: 'Samsung',
+      theme: 'light',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: 'ellie.png',
+    },
+    3: {
+      id: '3',
+      name: 'Ellie3',
+      company: 'Samsung',
+      theme: 'colorful',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+  });
 
-    const history = useHistory();
-    const onLogout = () => {
-        authService.logout();
-    };
+  const history = useHistory();
+  const onLogout = () => {
+    authService.logout();
+  };
 
-    useEffect(() => {
-        authService.onAuthChange(user => {
-            if(!user){
-                history.push('./');
-            }
-        });
+  useEffect(() => {
+    authService.onAuthChange(user => {
+      if (!user) {
+        history.push('/');
+      }
     });
+  });
 
-    const createOrUpdateCad = card => {
-        setCards(cards => {
-            const updated = {...cards};
-            updated[card.id] = card;
-            return updated;
-        });
-    };
+  const createOrUpdateCard = card => {
+    setCards(cards => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+      return updated;
+    });
+  };
 
-    const deleteCard = card => {
-        setCards(cards => {
-            const updated = {...cards};
-            delete updated[card.id];
-            return updated;
-        });
-    };
-    return(
-        <section className={styles.maker}>
-            <Header onLogout={onLogout}/>
-            <div className={styles.container}>
-                <Editor cards={cards} 
-                addCard={createOrUpdateCad} 
-                updateCard={createOrUpdateCad} 
-                deleteCard={deleteCard} />
-                <Preview cards={cards}/>
-            </div>
-            <Footer />
-        </section>
-    )
+  const deleteCard = card => {
+    setCards(cards => {
+      const updated = { ...cards };
+      delete updated[card.id];
+      return updated;
+    });
+  };
 
+  return (
+    <section className={styles.maker}>
+      <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor
+          FileInput={FileInput}
+          cards={cards}
+          addCard={createOrUpdateCard}
+          updateCard={createOrUpdateCard}
+          deleteCard={deleteCard}
+        />
+        <Preview cards={cards} />
+      </div>
+      <Footer />
+    </section>
+  );
 };
 
 export default Maker;
